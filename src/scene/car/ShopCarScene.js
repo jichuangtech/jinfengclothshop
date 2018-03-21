f83620d5c381de6084ef811e7d3dc0fa01e6ee88
huangbin <575395982@qq.com>
2018-03-21 17:09:40 +0800

    ADD Card界面添加 NavigationActions.dispatch()测试代码

--- src/scene/car/ShopCarScene.js
+++ src/scene/car/ShopCarScene.js
@@ -13,9 +13,9 @@ import {
     AsyncStorage,
     DeviceEventEmitter
 } from 'react-native';
-import { Checkbox} from 'antd-mobile'
+import {Checkbox} from 'antd-mobile'
 import {Align} from '../../css';
-import {CarInfoView,Divider} from "../../widget";
+import {CarInfoView, Divider} from "../../widget";
 import {Urls, NetUtils, ReduxProps, RespUtils} from "../../utils"
 import {connect} from 'react-redux';
 import {NavigationActions} from 'react-navigation';
@@ -29,7 +29,7 @@ class ShopCarScene extends React.Component {
         this.state = {
             isAllSelect: false,
             isRefreshing: false,
-            carts:[],
+            carts: [],
             isEditMode: false
         }
     }
@@ -40,70 +40,70 @@ class ShopCarScene extends React.Component {
         let editTitle = this.state.isEditMode ? "完成" : "编辑"
         return (
             <View style={CarStyle.container}>
-            <View style={{flexDirection: "row", alignItems: "center", marginTop:10}}>
-                <View style={{flex: 1}}>
-                    <Checkbox
-                              checked={isAllSelect}
-                              onChange={(event) => this.onAllSelectChange(event)}>
-                        金凤针织
-                    </Checkbox>
-                </View>
-
-                <View style={[{flex: 1}, Align.rLayout]}>
-                    <TouchableOpacity
-                        onPress={() => this.onEditBtnClick()}
-                    >
-                        <Text style={{marginRight: 10}}>{editTitle}</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-            <View style={{height: "85%"}}>
-                <FlatList
-                    numColumns={1}
-                    horizontal={false}
-                    onRefresh={() => this.refresh()}
-                    refreshing={this.state.isRefreshing}
-                    data={this.state.carts}
-                    renderItem={({item,index}) =>
-                        <CarInfoView
-                            key={index}
-                            cart={item.obj}
-                            isEditMode={this.state.isEditMode}
-                            isAllSelect={this.state.isAllSelect}
-                        />
-                    }
-                />
-            </View>
-
-            <Divider
-                style={[{width:"100%"}]}
-            />
-            <View style={{flex: 1, flexDirection: "row", alignItems: "center"}}>
-                <View style={[{flex: 1}, Align.lLayout]}>
-                    <Checkbox style={{marginLeft:10}}
+                <View style={{flexDirection: "row", alignItems: "center", marginTop: 10}}>
+                    <View style={{flex: 1}}>
+                        <Checkbox
                             checked={isAllSelect}
-                              onChange={(event) => this.onAllSelectChange(event)}>全选
-                    </Checkbox>
+                            onChange={(event) => this.onAllSelectChange(event)}>
+                            金凤针织
+                        </Checkbox>
+                    </View>
+
+                    <View style={[{flex: 1}, Align.rLayout]}>
+                        <TouchableOpacity
+                            onPress={() => this.onEditBtnClick()}
+                        >
+                            <Text style={{marginRight: 10}}>{editTitle}</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
 
-                <View style={[{flex: 1, height: "100%"}, Align.rLayout]}>
-                    <View style={[{flex: 1,}, Align.rLayout]}><Text style={[{marginRight:10}, Align.centerView]}>合计:{sum}</Text></View>
-                    <TouchableHighlight
-                        style={[{flex: 1, backgroundColor:"gray"}, Align.center]}
-                        underlayColor="#89FFC3"
-                        onPress={()=> this.pay()}
-                    >
-                        <Text>结算</Text>
-                    </TouchableHighlight>
+                <View style={{height: "85%"}}>
+                    <FlatList
+                        numColumns={1}
+                        horizontal={false}
+                        onRefresh={() => this.refresh()}
+                        refreshing={this.state.isRefreshing}
+                        data={this.state.carts}
+                        renderItem={({item, index}) =>
+                            <CarInfoView
+                                key={index}
+                                cart={item.obj}
+                                isEditMode={this.state.isEditMode}
+                                isAllSelect={this.state.isAllSelect}
+                            />
+                        }
+                    />
                 </View>
 
-            </View>
-        </View>);
+                <Divider
+                    style={[{width: "100%"}]}
+                />
+                <View style={{flex: 1, flexDirection: "row", alignItems: "center"}}>
+                    <View style={[{flex: 1}, Align.lLayout]}>
+                        <Checkbox style={{marginLeft: 10}}
+                                  checked={isAllSelect}
+                                  onChange={(event) => this.onAllSelectChange(event)}>全选
+                        </Checkbox>
+                    </View>
+
+                    <View style={[{flex: 1, height: "100%"}, Align.rLayout]}>
+                        <View style={[{flex: 1,}, Align.rLayout]}><Text style={[{marginRight: 10}, Align.centerView]}>合计:{sum}</Text></View>
+                        <TouchableHighlight
+                            style={[{flex: 1, backgroundColor: "gray"}, Align.center]}
+                            underlayColor="#89FFC3"
+                            onPress={() => this.pay()}
+                        >
+                            <Text>结算</Text>
+                        </TouchableHighlight>
+                    </View>
+
+                </View>
+            </View>);
     }
 
     onCarInfoCheckChange(index, checked) {
-        alert(" onCarInfoCheckChange index: " + index + ", checked: " + checked )
+        alert(" onCarInfoCheckChange index: " + index + ", checked: " + checked)
     }
 
     pay() {
@@ -112,7 +112,7 @@ class ShopCarScene extends React.Component {
 
     onAllSelectChange(event) {
         this.setState({
-          isAllSelect: event.target.checked
+            isAllSelect: event.target.checked
         })
     }
 
@@ -122,12 +122,12 @@ class ShopCarScene extends React.Component {
 
     onEditBtnClick() {
         this.setState({
-            isEditMode:!this.state.isEditMode
+            isEditMode: !this.state.isEditMode
         })
     }
 
     componentDidMount() {
-        if(this.isLogin()) {
+        if (this.isLogin()) {
             this.queryCart()
         } else {
             console.log("还没登录")
@@ -135,30 +135,41 @@ class ShopCarScene extends React.Component {
 
         DeviceEventEmitter.addListener('onTabShow',
             (value) => {
-                if(value !== Router.CART) {
+                if (value !== Router.CART) {
                     return
                 }
-                if(this.isLogin()) {
+                if (this.isLogin()) {
                     this.queryCart()
                 } else {
-                    const { navigate } = this.props.navigation;
+                    alert(" this.props.router1: " + JSON.stringify(this.props.navigation));
+                    const {navigate} = this.props.navigation;
                     // navigate('Login');
                     this.resetRouter();
+                    // this.dispatchRouter();
+                    alert(" this.props.router2: " + JSON.stringify(this.props.navigation));
                 }
                 // alert("ShopCart show: " + value)
             })
         DeviceEventEmitter.addListener('onTabHidden',
             (value) => {
-            // alert("ShopCart hidden: " + value)
-        })
+                // alert("ShopCart hidden: " + value)
+            })
+    }
+
+    dispatchRouter() {
+        const dispatchAction = NavigationActions.navigate({
+            routeName: 'Tab',
+            params: {},
+        });
+        this.props.navigation.dispatch(dispatchAction);
     }
 
     resetRouter() {
         const resetAction = NavigationActions.reset({
             index: 1,
             actions: [
-                NavigationActions.navigate({ routeName: 'Tab'}),
-                NavigationActions.navigate({ routeName: 'Login'})
+                NavigationActions.navigate({routeName: 'Tab'}),
+                NavigationActions.navigate({routeName: 'Login'})
             ]
         })
         this.props.navigation.dispatch(resetAction)
@@ -169,26 +180,30 @@ class ShopCarScene extends React.Component {
         let url = Urls.CART;
 
         AsyncStorage.getItem("access_token", function (error, value) {
-            if(error === null) {
+            if (error === null) {
                 NetUtils.getJson(url, value, null,
-                    (responseJson)=> {
+                    (responseJson) => {
                         self.updateCart(responseJson)
                     },
-                    (error)=>{alert("请求失败 error: " + error);},
-                    (catchError)=>{alert("捕获异常: " + catchError);})
+                    (error) => {
+                        alert("请求失败 error: " + error);
+                    },
+                    (catchError) => {
+                        alert("捕获异常: " + catchError);
+                    })
             }
         });
 
     }
 
     updateCart(cartJson) {
-        if(!RespUtils.isSuccess(cartJson.statusCode)) {
+        if (!RespUtils.isSuccess(cartJson.statusCode)) {
             console.log(" not success ");
             return
         }
         let carts = [];
         let a = 1;
-        for(let index in cartJson.data) {
+        for (let index in cartJson.data) {
             let cart = cartJson.data[index]
             let cartObj = {
                 key: index,
@@ -209,8 +224,8 @@ class ShopCarScene extends React.Component {
     }
 
     componentWillMount() {
-        if(this.props.loginProps.status !== "in") {
-            const { navigate } = this.props.navigation;
+        if (this.props.loginProps.status !== "in") {
+            const {navigate} = this.props.navigation;
             // alert("navigation: " + JSON.stringify(this.props.navigation))
             // navigate('Login');
         }
@@ -222,7 +237,7 @@ const CarStyle = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: "column",
-        backgroundColor:"white"
+        backgroundColor: "white"
     },
 
 
